850df77576156e4f5365754e9bcf0ce6642da172
110nard0 <76947677+110nard0@users.noreply.github.com>
2022-10-24 15:16:33 +0100

    Add macros

--- C-prog-lang/Chapter1/ex1-5.c
+++ C-prog-lang/Chapter1/ex1-5.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 /* print Fahrenheit-Celsius table in reverse */
 
 int main()
 {
 	int fahr;
 
-	for (fahr = 300; fahr >= 0; fahr -= 20)
+	for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
 		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
