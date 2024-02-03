fcb45ed42e83689211b2219df04cf146cac8cea8
psych0wh1z <ikeagudike@gmail.com>
2024-02-03 19:24:44 +0100

    Fix word delimiters block

--- C-prog-lang/Chapter1/character_IO/ex12.c
+++ C-prog-lang/Chapter1/character_IO/ex12.c
@@ -6,7 +6,7 @@ int main()
     int c;
 
     while ((c = getchar()) != EOF) {
-        if (c == ' ') {
+        if (c == ' ' || c == '\t' || c == '\n') {
             putchar('\n');
         }
         else {
---
+++ C-prog-lang/Chapter1/character_IO/ex13.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Print histogram of word lengths in input */
+
+int main()
+{
+    int c, 
+}
