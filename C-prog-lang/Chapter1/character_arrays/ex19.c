7c16a86337525e0fbb5b07d9da286ece41c131bf
psych0wh1z <ikeagudike@gmail.com>
2024-03-05 23:43:29 +0100

    Add reverse_string function

--- C-prog-lang/Chapter1/character_arrays/ex19.c
+++ C-prog-lang/Chapter1/character_arrays/ex19.c
@@ -8,7 +8,28 @@ int main()
     int;
 }
 
-/* reverse character string */
-int reverse_line(char s[])
+/**
+ * rev_string -  reverse input string
+ *
+ * @s: variable holding the pointer
+ * Return: void
+ */
+void reverse_string(char s[])
 {
+    int len, i, mid;
+    char temp;
+
+    for (len = 0; s[len] != '\0'; len++)
+        ;
+
+    i = 0;
+    mid = len / 2;
+
+    while (mid--)
+    {
+        temp = s[len - 1 - i];
+        s[len - i - 1] = s[i];
+        s[i] = temp;
+        i++;
+    }
 }
