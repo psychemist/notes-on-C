fb9c9f834fc0942b26c451d3d8b373ba389a22f3
psych0wh1z <ikeagudike@gmail.com>
2024-03-06 21:14:22 +0100

    Write main function for string reversal

--- C-prog-lang/Chapter1/character_arrays/ex19.c
+++ C-prog-lang/Chapter1/character_arrays/ex19.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 
-int reverse_line(char s[]);
+#define MAXLINE 1000 /* maximum input line size */
+
+int get_line(char line[], int maxline);
+void reverse_line(char s[]);
 
 /* reverses character input a line at a time */
 int main()
 {
-    int;
+    int len;            /* current line length */
+    int nums;           /* number of lines above character limit */
+    char line[MAXLINE]; /* current input line */
+
+    nums = 0;
+
+    while ((len = get_line(line, MAXLINE)) > 0)
+    {
+        reverse_line(line);
+        printf("%s\n", line);
+    }
+
+    return 0;
+}
+
+/* get_line: read a line into s, return length */
+int get_line(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
 
 /**
@@ -14,7 +45,7 @@ int main()
  * @s: variable holding the pointer
  * Return: void
  */
-void reverse_string(char s[])
+void reverse_line(char s[])
 {
     int len, i, mid;
     char temp;
