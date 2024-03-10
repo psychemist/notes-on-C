36c6a27894ad08587ddef4961e4987326a1afa64
psych0wh1z <ikeagudike@gmail.com>
2024-03-10 23:36:43 +0100

    Update function signature

--- C-programming_language/Chapter1/character_arrays/ex18.c
+++ C-programming_language/Chapter1/character_arrays/ex18.c
@@ -2,7 +2,7 @@
 
 #define MAXLINE 1000 /* maximum input line size */
 
-int clear_trailing_whitespace(char s[]);
+int clear_trailing_whitespace(char s[], int len);
 int get_line(char s[], int lim);
 
 /* remove trailing blanks and tabs from input lines and delete blank lines */
@@ -13,7 +13,7 @@ int main()
 
     while ((len = get_line(line, MAXLINE)) > 0)
     {
-        cleaned_line = clear_trailing_whitespace(char s[], len);
+        printf("%s", clear_trailing_whitespace(line, len));
     }
 }
 
@@ -33,10 +33,10 @@ int get_line(char s[], int lim)
     return i;
 }
 
-int clear_trailing_whitespace(char s[])
+int clear_trailing_whitespace(char s[], int len)
 {
     int i;
-    
+
     for (i = 0; i < len; i++)
     {
     }
