0b491f4362266c687eb66261289da56c5676ee7b
psych0wh1z <ikeagudike@gmail.com>
2024-03-11 04:24:42 +0100

    Add comments to code

--- C-programming_language/Chapter1/character_arrays/ex18.c
+++ C-programming_language/Chapter1/character_arrays/ex18.c
@@ -8,47 +8,52 @@ int get_line(char s[], int lim);
 /* remove trailing blanks and tabs from input lines and delete blank lines */
 int main()
 {
-    int len;
-    char line[MAXLINE];
+	int len;
+	char line[MAXLINE];
 
-    while ((len = get_line(line, MAXLINE)) > 0)
-    {
-        clear_trailing_whitespace(line, len);
-        printf("%s\n", line);
-    }
+	while ((len = get_line(line, MAXLINE)) > 0)
+	{
+		clear_trailing_whitespace(line, len);
+		printf("%s\n", line);
+	}
 }
 
 /* get_line: read a line into s, return length */
 int get_line(char s[], int lim)
 {
-    int c, i;
+	int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        s[i] = c;
-    if (c == '\n')
-    {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-    return i;
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
+	{
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
 }
 
+/* remove trailing blanks and tabs from strings and discard empty strings */
 void clear_trailing_whitespace(char *s, int len)
 {
-    int i;
+	int i;
 
-    for (i = len - 1; i > 0; i--)
-    {
-        if (s[i + 1] == '\0')
-        {
-            if (s[i] == '\t' || s[i] == ' ' || s[i] == '\n')
-            {
-                s[i] = '\0';
-            }
-        }
-    }
+	// loop over line starting from character before null terminator
+	for (i = len - 1; i > 0; i--)
+	{
+		// check if next character is a null pointer (at last valid index)
+		if (s[i + 1] == '\0')
+		{
+			// if character is a tab or blank, change it to a null character
+			if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+			{
+				s[i] = '\0';
+			}
+		}
+	}
 
-    if (i == 0 && (s[i] == '\t' || s[i] == ' ') && s[i + 1] == '\0')
-        s[i] = '\0';
+	// check if first and last character in line is a tab or blank and delete
+	if (i == 0 && (s[i] == '\t' || s[i] == ' ') && s[i + 1] == '\0')
+		s[i] = '\0';
 }
