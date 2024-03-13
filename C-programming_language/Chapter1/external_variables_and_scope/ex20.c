0ceb3da2f7044a971d79d8205a9019436c3439e2
psych0wh1z <ikeagudike@gmail.com>
2024-03-13 23:11:44 +0100

    Add get_lne function

--- C-programming_language/Chapter1/external_variables_and_scope/ex20.c
+++ C-programming_language/Chapter1/external_variables_and_scope/ex20.c
@@ -7,6 +7,25 @@ int main ()
 {
 }
 
+/* get_line: read a line into s, return length */
+int get_line(char s[], int lim)
+{
+    int c, i;
+
+    // loop over line input and store characters in buffer string
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    // add null terminator after new line character
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+
 int detab ()
 {
 }
