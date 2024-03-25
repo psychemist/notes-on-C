3ccdec56d3a9d6da13c333cdb89108cd8e01ad0b
psych0wh1z <ikeagudike@gmail.com>
2024-03-25 08:25:49 +0100

    Test detab function

--- C-programming_language/Chapter1/external_variables_and_scope/ex20.c
+++ C-programming_language/Chapter1/external_variables_and_scope/ex20.c
@@ -3,17 +3,21 @@
 #define TABSPACES 4  /* number of columns in a set of tab stops */
 #define MAXLINE 1000 /* maximum input line size */
 
-int detab(char s[]);
+int get_line(char s[], int lim);
+char *detab(char s[]);
 
+/* change tabs in input line to spaces */
 int main()
 {
     int len, lens;
     char line[MAXLINE];
+    char spaced_line[MAXLINE];
 
     // check that line is not blank
     while ((len = get_line(line, MAXLINE)) > 0)
     {
-        detab(line);
+        spaced_line = detab(line);
+        printf("%s", spaced_line);
     }
 
     return 0;
@@ -27,6 +31,7 @@ int get_line(char s[], int lim)
     // loop over line input and store characters in buffer string
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         s[i] = c;
+
     // add null terminator after new line character
     if (c == '\n')
     {
@@ -34,9 +39,32 @@ int get_line(char s[], int lim)
         ++i;
     }
     s[i] = '\0';
+
     return i;
 }
 
-int detab(char s[])
+/* replace tabs in input with blanks to space to the next tab stop */
+char *detab(char s[])
 {
+    char new_line[MAXLINE];
+    int i = 0;
+
+    while (s[i] != '\0')
+    {
+        new_line[i] = s[i];
+
+        if (s[i] == '\t')
+        {
+            for (int j = 0; j < TABSPACES; j++)
+            {
+                new_line[i] = '\b';
+            }
+            i - TABSPACES;
+        }
+
+        i++;
+    }
+    new_line[i] = '\0';
+
+    return new_line;
 }
